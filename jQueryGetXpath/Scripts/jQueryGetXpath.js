2f8ccdb2f7de0011428f92289c6bbc427982ebba
RiMaDhiWJi <xd23746mad858797@126.com>
2016-09-02 14:58:00 +0800

    优化获取xpath 添加 获取xpathindex 方法

--- jQueryGetXpath/Scripts/jQueryGetXpath.js
+++ jQueryGetXpath/Scripts/jQueryGetXpath.js
@@ -1,14 +1,15 @@
-﻿; (function ($, window, document, undefined) {
+; (function ($, window, document, undefined) {
     var pluginName = "jQueryGetXpath",  //自定义一个插件名称
-        defaults = {                //定义插件的默认属性
 
+        defaults = {                //定义插件的默认属性
+            selfIdMode: true
         };
 
     var getXpath = function (xpath, xSelector, options) {
         // 获取Xpath
         var $this = xSelector;
         var tag = $this.get(0).tagName.toLowerCase();
-        //console.log(tag + ":" + $this.index());
+
         if (tag === "body") {
             return "//body" + xpath;
         }
@@ -22,7 +23,7 @@
                     xpath = ("/" + tag + "[@class=\"" + $this.attr("class") + "\"]") + xpath;
                     return "/" + xpath;
                 } else {
-                    xpath = ("/" + tag + "[@class=\"" + $this.attr("class") + "\"]") + "[" + ($this.index() + 1) + "]" + xpath;
+                    xpath = ("/" + tag + "[@class=\"" + $this.attr("class") + "\"]") + "[" + ($this.jQueryGetXpath("getXpathIndex")) + "]" + xpath;
                     if ($this.parent().length) {
                         xSelector = $this.parent();
                         return getXpath(xpath, xSelector, options);
@@ -30,7 +31,7 @@
                     return "/" + xpath;
                 }
             } else {
-                xpath = ("/" + tag) + "[" + ($this.index() + 1) + "]" + xpath;
+                xpath = ("/" + tag) + "[" + ($this.jQueryGetXpath("getXpathIndex")) + "]" + xpath;
                 if ($this.parent().length) {
                     xSelector = $this.parent();
                     return getXpath(xpath, xSelector, options);
@@ -52,7 +53,7 @@
                     xpath = ("/" + tag + "[@class=\"" + $this.attr("class") + "\"]") + xpath;
                     return "/" + xpath;
                 } else {
-                    xpath = ("/" + tag + "[@class=\"" + $this.attr("class") + "\"]") + "[" + ($this.index() + 1) + "]" + xpath;
+                    xpath = ("/" + tag + "[@class=\"" + $this.attr("class") + "\"]") + "[" + ($this.jQueryGetXpath("getXpathIndex")) + "]" + xpath;
                     if ($this.parent().length) {
                         xSelector = $this.parent();
                         return getXpath(xpath, xSelector, options);
@@ -60,7 +61,7 @@
                     return "/" + xpath;
                 }
             } else {
-                xpath = ("/" + tag) + "[" + ($this.index() + 1) + "]" + xpath;
+                xpath = ("/" + tag) + "[" + ($this.jQueryGetXpath("getXpathIndex")) + "]" + xpath;
                 if ($this.parent().length) {
                     xSelector = $this.parent();
                     return getXpath(xpath, xSelector, options);
@@ -88,37 +89,82 @@
         },
         getXpath: function (options) {
             var $this = $(this);
-            return getXpath("", $this, options);
+            var defaults = {                //定义插件的默认属性
+                selfIdMode: true,
+                keepTbody: false,
+                isHightLight: true
+            };
+            var settings = $.extend({}, defaults, options);
+            var xpath = getXpath("", $this, settings);
+            if (!settings.keepTbody) {
+                xpath = xpath.replace(/\/tbody[0-9\[\]^/]*\//g, "/");
+            }
+            if (settings.isHightLight) {
+                $this.css("outline", "1px solid #" + (~~(Math.random() * (1 << 24))).toString(16));
+            }
+            return xpath;
         },
         getSelector: function (options) {
             // good
+
+
+        },
+        getXpathIndex: function() {
+            var $this = $(this);
+            var tag = $this.get(0).tagName;
+            var $parents = $this.parent();
+            var $siblings = $parents.children(tag);
+            var tlength = $siblings.length;
+
+            for (var i = 1; i <= tlength; i++) {
+                if ($siblings.get([i - 1]) === $this.get(0)) {
+                    return i;
+                }
+            }
+
         }
     };
 
     $.fn[pluginName] = function () {
         // 获取传入的方法，切勿用function(method){}来实现，否则会毁掉一切
+
+
         // arguments对象用法参考：http://www.w3school.com.cn/js/pro_js_functions_arguments_object.asp
+
+
         var method = arguments[0];
         // 方法调用
+
+
         if (methods[method]) {
 
             // 如果存在，获取方法名
+
+
             method = methods[method];
 
             // 我们的方法是作为参数传入的，把它从参数列表中删除，因为调用方法时仅需JSON格式参数，并不需要它
+
+
             arguments = Array.prototype.slice.call(arguments, 1);
 
         } else if (typeof method === 'object' || !method) {
 
             // 如果没有传入方法名，则默认调用init方法，参数为arguments
+
+
             method = methods.init;
         } else {
 
             // 否则提示错误
+
+
             $.error('Method' + method + 'does not exist on jQuery.tooltip');
         }
 
         // 用apply方法来调用方法，并传入参数 
+
+
         return method.apply(this, arguments);
     };
 
